5b8cbd7f154ea51a8f27a1f944ab9a8031d67897
L3KK1 <V.L-2018@yandex.ru>
2026-02-27 14:19:45 +0300

    +TableItem

---
+++ frontend/src/components/TableItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr>
+    <td>{{ book.book_id }}</td>
+    <td>{{ book.title }}</td>
+    <td>{{ book.publication_year }}</td>
+    <td>{{ book.publisher }}</td>
+    <td>{{ book.genre_id }}</td>
+    <td>{{ book.page_count }}</td>
+    <td>{{ book.description }}</td>
+  </tr>
+</template>
+
+<script setup>
+defineProps({
+  book: {
+    type: Object,
+    required: true
+  }
+})
+</script>
